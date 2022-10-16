acf1d447d4420ccacba25dd396bbe70c120a0b92
Alexander <alexanderwuensche@hotmail.com>
2022-10-16 10:15:57 +0200

    BST with dict

--- Binary Search Tree/BST with dict.py
+++ Binary Search Tree/BST with dict.py
@@ -1,45 +1,51 @@
 class Node:
-    def __init__(self, data):
+    def __init__(self, key, data):
         self.left = None
         self.right = None
+        self.key = key
         self.data = data
+        self.combo = {self.key: self.data}
 
     # Insert method to create nodes
-    def insert(self, data):
-        if self.data:
-            if data < self.data:
+    def insert(self, key, data):
+        if self.key:
+            if key < self.key:
                 if self.left is None:
-                    self.left = Node(data)
+                    self.left = Node(key, data)
                 else:
-                    self.left.insert(data)
-            elif data > self.data:
+                    self.left.insert(key, data)
+            elif key > self.key:
                 if self.right is None:
-                    self.right = Node(data)
+                    self.right = Node(key, data)
                 else:
-                    self.right.insert(data)
+                    self.right.insert(key, data)
         else:
+            self.key = key
             self.data = data
 
-    # findval method to compare the value with nodes
-    def get(self, lkpval):
-        if lkpval < self.data:
+    # get method to compare the value with nodes
+    def get(self, name):
+        if name < self.key:
             if self.left is None:
-                return str(lkpval) + " Not Found"
-            return self.left.get(lkpval)
-        elif lkpval > self.data:
+                return str(name) + " Not Found"
+            return self.left.get(name)
+        elif name > self.key:
             if self.right is None:
-                return str(lkpval) + " Not Found"
-            return self.right.get(lkpval)
+                return str(name) + " Not Found"
+            return self.right.get(name)
         else:
-            print(str(self.data) + ' is found')
+            print(str(self.data))
 
 
-root = Node("Vahe")
-root.insert("Thomas")
-root.insert("Zeke")
-root.insert("Al")
-root.insert("Vahe")  # is skipped as it is already implemented
-root.insert("Vahy")
-print(root.get("Thomas"))
-# print(root.get(14))
-# print(root.get(3))
+root = Node("Vahe", 55)
+root.insert("Thomas", 45)
+root.insert("Zeke", 123)
+root.insert("Al", 23)
+root.insert("Vahy", 66)
+#root.insert("Vahe")  # is skipped as it is already implemented
+
+root.get("Thomas")
+root.get("Vahe")
+root.get("Zeke")
+root.get("Al")
+root.get("Alex")
