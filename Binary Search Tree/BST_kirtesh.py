621033483872fe753ed682ae48623c0da5a08ef7
Alexander <alexanderwuensche@hotmail.com>
2022-10-22 19:17:57 +0200

    Added docstrings

--- Binary Search Tree/BST_kirtesh.py
+++ Binary Search Tree/BST_kirtesh.py
@@ -1,5 +1,11 @@
 class Node:
     def __init__(self, key, value):
+        """
+        Initializes the Node class with a Name (key) and an Age (value) so that it acts like a dictionary when adding
+        nodes left or right in the Tree.
+        :param key: String, which takes a Name
+        :param value: Integer which takes an age
+        """
         # this variable accounts for values that are lesser than the key value
         self.left_child = None
         # this variable accounts for the values that are larger than the key value
@@ -11,6 +17,14 @@ class Node:
     # Insert method to create nodes
     # this function is used to enter the remaining elements ( other than the node value) of the list,
     def insert(self, key, value):
+        """
+        Calling this function on the Node will insert the Name (key) as a child either left or right to the Node. The
+        sorting is dictated by the index in the alphabet, i.e. if the root Node is "Vahe", names with a "lower" rank
+        like "Thomas" will be inserted as a child to the left, because "T" is ranked lower than "V".
+        :param key: String, which takes a Name
+        :param value: Integer which takes an age
+        :return: returns nothing, changes child nodes
+        """
         if self.key:  # this section will be executed if key value of node is already defined during initialization
             if key < self.key:  # if key value defined in the insert function is less than the key value defined during initialization, this section will be executed
                 if self.left_child is None:  # if left_child is being defined for the first time, this section will be executed
@@ -28,7 +42,7 @@ class Node:
                 else:
                     # a recursive function of insert is defined, so the alrogithm will run back though the insert function and the value will be stored in the subclass of initially defined left_child value, so as to form a binary tree
                     self.right_child.insert(key, value)
-        else:                                          # this section will be executed if key value of node has not been defined
+        else:  # this section will be executed if key value of node has not been defined
             # this is to define key value of node if it has not been defined during initialization
             self.key = key
             self.value = value
@@ -37,23 +51,27 @@ class Node:
     # get method to compare the value with nodes
 
     def get(self, name):
+        """
+        The get function inside Node class returns the corresponding age of the Name passed, which is saved as a
+        value to its respective key, like a dictionary. The get function will search the BST and compare the searched
+        name to the children names, to determine if the searched name is found in the left or right child. Once it has
+        found the name, it will return its corresponding age.
+        :param name: A string
+        :return: Returns the corresponding age to the passed name, or, if the name is not found, will return
+        "Name not found"
+        """
         # if node key greater than the input string key ( from get function ), then this section will be executed, ( pls note key is string datatype, so comparison will be based on their alphabatical order )
         if name < self.key:
             if self.left_child is None:  # this line is used to present an error message, if left key of node is empty then the get key is not present in the tree
-                return str(
-                    name) + " Not Found"  # returns the error message that, the key we are looking for is not present in the tree
-            return self.left_child.get(
-                name)  # a recursion function which takes left_child key as node and reruns the get function with left_child key as base
+                return str(name) + " Not Found"  # returns the error message that, the key we are looking for is not present in the tree
+            return self.left_child.get(name)  # a recursion function which takes left_child key as node and reruns the get function with left_child key as base
         # if node key less than the input string key ( from get function ), then this section will be executed, ( pls note key is string datatype, so comparison will be based on their alphabatical order )
         elif name > self.key:
             if self.right_child is None:  # this line is used to present an error message, if right key of node is empty then the get key is not present in the tree
-                return str(
-                    name) + " Not Found"  # returns the error message that, the key we are looking for is not present in the tree
-            return self.right_child.get(
-                name)  # a recursion function which takes left_child key as node and reruns the get function with left_child key as base
+                return str(name) + " Not Found"  # returns the error message that, the key we are looking for is not present in the tree
+            return self.right_child.get(name)  # a recursion function which takes left_child key as node and reruns the get function with left_child key as base
         else:
-            return str(
-                self.value)  # if none of the if and else if conditions stated above are true, then the node key we are comparing is the get key value we are looking for, and so the algorithm will return the corresponding value of the get key
+            return str(self.value)  # if none of the if and else if conditions stated above are true, then the node key we are comparing is the get key value we are looking for, and so the algorithm will return the corresponding value of the get key
 
     # similarly we are searching for the corresponding value of the key by going through the branches step by step and comparing the node values with the input key
 
