8fac27a0c1b397ac3e7a4cd43178743757e354be
Alexander <alexanderwuensche@hotmail.com>
2022-10-22 21:29:39 +0200

    final edits

--- Binary Search Tree/BST_kirtesh.py
+++ Binary Search Tree/BST_kirtesh.py
@@ -12,7 +12,7 @@ class Node:
         self.right_child = None
         self.key = key  # key of node is defined during the initialization
         self.value = value  # value of node which is the corresponding value of the key
-        #self.combo = {self.key: self.value}
+        #self.combo = {self.key: self.value}  # this was the initial thought, to make a dictionary
 
     # Insert method to create nodes
     # this function is used to enter the remaining elements ( other than the node value) of the list,
@@ -28,13 +28,14 @@ class Node:
         if self.key:  # this section will be executed if key value of node is already defined during initialization
             if key < self.key:  # if key value defined in the insert function is less than the key value defined
                 # during initialization, this section will be executed
-                if self.left_child is None:  # if left_child is being defined for the first time, this section will be executed
-                    # left_child variable defined during initialization will be assigned key value defined in insert function
-                    self.left_child = Node(key, value)
-                # if left_child value has already been taken (i.e it has already been defined beforehead by calling
+                if self.left_child is None:  # if left_child is being defined for the first time, this section will
+                    # be executed
+                    self.left_child = Node(key, value)  # left_child variable defined during initialization will be
+                    # assigned key value defined in insert function
+                # if left_child value has already been taken (i.e. it has already been defined beforehand by calling
                 # insert function), this section will be executed
                 else:
-                    # a recursive function of insert is defined, so the alrogithm will run back though the insert
+                    # a recursive function of insert is defined, so the algorithm will run back though the insert
                     # function and the value will be stored in the subclass of initially defined left_child value,
                     # so as to form a binary tree
                     self.left_child.insert(key, value)
@@ -44,16 +45,18 @@ class Node:
                     # be executed
                     self.right_child = Node(key, value)  # right_child variable defined during initialization will be
                     # assigned key value defined in insert function
-                else:  # if right_child value has already been taken (i.e it has already been defined beforehead by
+                else:  # if right_child value has already been taken (i.e., it has already been defined beforehand by
                     # calling insert function), this section will be executed
                     self.right_child.insert(key, value)  # a recursive function of insert is defined, so the
-                    # alrogithm will run back though the insert function and the value will be stored in the subclass
+                    # algorithm will run back though the insert function and the value will be stored in the subclass
                     # of initially defined left_child value, so as to form a binary tree
         else:  # this section will be executed if key value of node has not been defined
             # this is to define key value of node if it has not been defined during initialization
             self.key = key
             self.value = value
-    # basically in this algorithm we are creating binary branches about a node key, and are assigning one right and one left node key to that branch, and subsequently we are creating more binary branches to the left and right key ( for which recursion method is used ) which can be called subbranches of the main branch and so on...
+    # basically in this algorithm we are creating binary branches about a node key, and are assigning one right and
+    # one left node key to that branch, and subsequently we are creating more binary branches to the left and right
+    # key ( for which recursion method is used ) which can be called subbranches of the main branch and so on...
 
     # get method to compare the value with nodes
 
@@ -67,20 +70,31 @@ class Node:
         :return: Returns the corresponding age to the passed name, or, if the name is not found, will return
         "Name not found"
         """
-        # if node key greater than the input string key ( from get function ), then this section will be executed, ( pls note key is string datatype, so comparison will be based on their alphabatical order )
-        if name < self.key:
-            if self.left_child is None:  # this line is used to present an error message, if left key of node is empty then the get key is not present in the tree
-                return str(name) + " Not Found"  # returns the error message that, the key we are looking for is not present in the tree
-            return self.left_child.get(name)  # a recursion function which takes left_child key as node and reruns the get function with left_child key as base
-        # if node key less than the input string key ( from get function ), then this section will be executed, ( pls note key is string datatype, so comparison will be based on their alphabatical order )
-        elif name > self.key:
-            if self.right_child is None:  # this line is used to present an error message, if right key of node is empty then the get key is not present in the tree
-                return str(name) + " Not Found"  # returns the error message that, the key we are looking for is not present in the tree
-            return self.right_child.get(name)  # a recursion function which takes left_child key as node and reruns the get function with left_child key as base
+        if name < self.key:  # if node key greater than the input string key ( from get function ), then this section
+            # will be executed, ( pls note key is string datatype, so comparison will be based on their alphabetical
+            # order )
+            if self.left_child is None:  # this line is used to present an error message, if left key of node is
+                # empty then the get key is not present in the tree
+                return str(name) + " Not Found"  # returns the error message that, the key we are looking for is not
+                # present in the tree
+            return self.left_child.get(name)  # a recursion function which takes left_child key as node and reruns
+            # the get function with left_child key as base
+        elif name > self.key:  # if node key less than the input string key ( from get function ), then this section
+            # will be executed, ( pls note key is string datatype, so comparison will be based on their alphabetical
+            # order )
+            if self.right_child is None:  # this line is used to present an error message, if right key of node is
+                # empty then the get key is not present in the tree
+                return str(name) + " Not Found"  # returns the error message that, the key we are looking for is not
+                # present in the tree
+            return self.right_child.get(name)  # a recursion function which takes left_child key as node and reruns
+            # the get function with left_child key as base
         else:
-            return str(self.value)  # if none of the if and else if conditions stated above are true, then the node key we are comparing is the get key value we are looking for, and so the algorithm will return the corresponding value of the get key
+            return str(self.value)  # if none of the if and else if conditions stated above are true, then the node
+            # key we are comparing is the get key value we are looking for, and so the algorithm will return the
+            # corresponding value of the get key
 
-    # similarly we are searching for the corresponding value of the key by going through the branches step by step and comparing the node values with the input key
+    # similarly we are searching for the corresponding value of the key by going through the branches step by step
+    # and comparing the node values with the input key
 
 
 tree = Node("Vahe", 55)
